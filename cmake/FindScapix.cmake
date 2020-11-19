18e0b6d7fb6c4976cb718981f63f29f7df6704d5
elad.haviv <elad.haviv@live.com>
2020-11-19 13:45:16 +0200

    update scapix

--- cmake/FindScapix.cmake
+++ cmake/FindScapix.cmake
@@ -1,7 +1,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/cmodule.cmake)
 
 cmodule_add(
-  scapix 1.0.16
-  URL "https://github.com/scapix-com/scapix/archive/v1.0.16.tar.gz"
-  URL_HASH SHA256=67e5854d8df20807c7999dd3cd0c281421d2f044632e601a583f7943efa10e5f
+  scapix 1.0.17-adshir
+  URL "https://github.com/Adshir/scapix/archive/v1.0.17-adshir.tar.gz"
+  URL_HASH SHA256=26b12001378b681ba2b8f90d3b29020e8e748777e28c784962d5682d979ce2d0
 )
